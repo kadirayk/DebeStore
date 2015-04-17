4f534b2575d96403e0b1b33d9f233ecfc6296600
kadirayk <kadiray.k@hotmail.com>
2015-04-17 22:50:04 +0300

    add createDebeDetail insert method

--- app/src/main/java/com/kadirayk/debestore/database/DebeDetailDataSource.java
+++ app/src/main/java/com/kadirayk/debestore/database/DebeDetailDataSource.java
@@ -1,7 +1,12 @@
 package com.kadirayk.debestore.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.provider.ContactsContract;
+
+import com.kadirayk.debestore.models.DebeDetailItem;
 
 /**
  * Created by Kadiray on 16.04.2015.
@@ -29,5 +34,30 @@ public class DebeDetailDataSource {
 
     public void close(){
         dbHelper.close();
+    }
+
+    public DebeDetailItem createDebeDetail(int parentId, int place, String title, String author, String url, String content, String date){
+        title = title.trim();
+        author = author.trim();
+        url = url.trim();
+        date = date.trim();
+
+        ContentValues values = new ContentValues();
+        values.put(DatabaseHelper.DEBE_DETAIL_COLUMN_PARENT_ID, parentId);
+        values.put(DatabaseHelper.DEBE_DETAIL_COLUMN_PLACE, place);
+        values.put(DatabaseHelper.DEBE_DETAIL_COLUMN_TITLE, title);
+        values.put(DatabaseHelper.DEBE_DETAIL_COLUMN_AUTHOR, author);
+        values.put(DatabaseHelper.DEBE_DETAIL_COLUMN_URL, url);
+        values.put(DatabaseHelper.DEBE_DETAIL_COLUMN_CONTENT, content);
+        values.put(DatabaseHelper.DEBE_DETAIL_COLUMN_DATE, date);
+
+        long insertId = database.insert(DatabaseHelper.TABLE_DEBE_DETAIL, null, values);
+        Cursor cursor = database.query(DatabaseHelper.TABLE_DEBE_DETAIL,
+                allColumns, DatabaseHelper.DEBE_COLUMN_ID + " = " + insertId, null,
+                null, null, null);
+        cursor.moveToFirst();
+        DebeDetailItem debeDetailItem = null; //TODO = cursorTODebeDetailItem(cursor);
+        cursor.close();
+        return debeDetailItem;
     }
 }
